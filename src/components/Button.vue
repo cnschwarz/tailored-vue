97e0d737a68144cada4eb602b1b604d076baaf91
Colin Schwarz <cnschwarz@users.noreply.github.com>
2021-06-05 21:18:54 +0200

    chore(setup): first draft for library config

---
+++ src/components/Button.vue
@@ -0,0 +1,49 @@
+<template>
+  <button
+    class="flex flex-row items-center justify-between gap-1 p-1 cursor-pointer
+    transition-colors duration-200 ease-out"
+    :name="name"
+    @click="clickHandler">
+    {{ label }}
+    <slot />
+  </button>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Button',
+  components: {},
+  props: {
+    icon: {
+      type: String,
+    },
+    icon2: {
+      type: String,
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+    name: {
+      type: String,
+      default: () => 'button1',
+    },
+    readonly: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  emits: ['click'],
+  setup(props, { emit }) {
+    const clickHandler = () => {
+      if (!props.readonly) {
+        emit('click');
+      }
+    };
+    return { clickHandler };
+  },
+});
+</script>
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import Button from './components/Button.vue';
+import Select from './components/Select.vue';
+import './styles/main.css';
+
+export {
+  Button,
+  Select,
+};
